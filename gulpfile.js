0327fd1e9b607bd47861a15c79f809064482c7ed
Charles Robertson <admin@cocohost.co>
2019-01-08 22:07:35 -0600

    Set up Gulp, Pug, Stylus, and BrowserSync

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+var gulp = require('gulp');
+var pug = require('gulp-pug');
+var stylus = require('gulp-stylus');
+var browserSync = require('browser-sync').create();
+
+var site = 'public/';
+
+// Compile Pug files into HTML
+gulp.task('pug', function () {
+  return gulp.src('src/pug/index.pug')
+    .pipe(pug())
+    .pipe(gulp.dest(site));
+});
+
+// Compile Stylus files into CSS
+gulp.task('stylus', function () {
+  return gulp.src('src/stylus/style.styl')
+    .pipe(stylus({
+      'include css': true
+    }))
+    .pipe(gulp.dest(site))
+    .pipe(browserSync.stream());
+});
+
+// // browserSync and file watching
+gulp.task('serve', function() {
+
+  browserSync.init({
+      server: site,
+      browser: 'FireFox Developer Edition'
+  });
+
+  gulp.watch("src/stylus/*.styl", gulp.series('stylus'));
+  gulp.watch("src/pug/*.pug", gulp.series('pug'));
+  gulp.watch(site + "*.html").on('change', browserSync.reload);
+});
+
+  
+// });
+
+// gulp.task('default', ['watch']);
