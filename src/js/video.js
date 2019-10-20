b2820ac939cc703535f6cc978d95248483b7404f
Charles Robertson <admin@cocohost.co>
2019-10-20 18:26:13 -0500

    Start new controls

--- src/js/video.js
+++ src/js/video.js
@@ -1,4 +1,15 @@
-const videos = document.getElementsByTagName('video');
-console.log(videos);
+/* Get our Elements */
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const ranges = player.querySelectorAll('.player__slider');
 
-// for(let i = 0; i < videos.length;)
+/* Build out functions */
+function togglePlay() {
+    const method = video.paused ? 'play' : 'pause'
+    video[method]();
+}
+
+/* Hook up the event listeners */
