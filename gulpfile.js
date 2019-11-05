4aa55714e97d9eac9828835e54c520e43bca5b99
Charles Robertson <admin@cocohost.co>
2019-11-05 13:08:49 -0600

    Setup Gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,60 @@
 /* eslint-disable semi */
 'use strict';
 
-var gulp = require('gulp');
-var pug = require('gulp-pug');
-var uglify = require('gulp-uglify');
-var stylus = require('gulp-sass');
-var browserSync = require('browser-sync').create();
-const sourcemaps = require('gulp-sourcemaps');
+const gulp = require('gulp');
+const pug = require('gulp-pug');
+const sass = require('gulp-sass');
 const babel = require('gulp-babel');
+const sourcemaps = require('gulp-sourcemaps');
+const uglify = require('gulp-uglify');
+const browserSync = require('browser-sync').create();
 // const concat = require('gulp-concat');
-
-var site = 'public/';
 
 // Copy .htaccess file to dist to force HTTPS
 gulp.task('root', function () {
   return gulp.src('src/root/.htaccess')
-    .pipe(gulp.dest(site));
+    .pipe(gulp.dest('dist'));
+});
+
+// Copy favicons to dist
+gulp.task('favicons', function () {
+  return gulp.src('src/favicons/*')
+    .pipe(gulp.dest('dist/favicons'));
+});
+
+// Copy fonts to dist
+gulp.task('fonts', function () {
+  return gulp.src('src/fonts/*')
+    .pipe(gulp.dest('dist/fonts'));
+});
+
+// Copy images to dist
+gulp.task('images', function () {
+  return gulp.src('src/images/*')
+    .pipe(gulp.dest('dist/images'));
+});
+
+// Copy videos to dist
+gulp.task('videos', function () {
+  return gulp.src('src/videos/*')
+    .pipe(gulp.dest('dist/videos'));
+});
+
+// Compile Pug files into HTML
+gulp.task('pug', function () {
+  return gulp.src('src/pug/index.pug')
+    .pipe(pug())
+    .pipe(gulp.dest('dist'));
+});
+
+// Compile SCSS files to CSS
+sass.compiler = require('node-sass');
+
+gulp.task('sass', function () {
+  return gulp.src('src/sass/style.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('dist'))
+    .pipe(browserSync.stream());
 });
 
 // JavaScript
@@ -26,73 +65,21 @@ gulp.task('js', () =>
       presets: ['@babel/preset-env']
     }))
     .pipe(uglify())
-    .pipe(gulp.dest(site + '/js'))
+    .pipe(gulp.dest('dist/js'))
 );
-
-// gulp.task('uglify', function () {
-//   return gulp.src('src/js/*.js')
-//     .pipe(uglify())
-//     .pipe(gulp.dest(site + '/js'))
-// });
-
-// Compile Pug files into HTML
-gulp.task('pug', function () {
-  return gulp.src('src/pug/index.pug')
-    .pipe(pug())
-    .pipe(gulp.dest(site));
-});
-
-// Compile Stylus files into CSS
-gulp.task('stylus', function () {
-  return gulp.src('src/stylus/style.styl')
-    .pipe(stylus({
-      'include css': true
-    }))
-    .pipe(gulp.dest(site))
-    .pipe(browserSync.stream());
-});
-
-// Clean SVGs
-gulp.task('svgo', () => {
-  return gulp.src('src/images/*.svg')
-    .pipe(svgo(
-      {
-        plugins: [{
-          removeTitle: false
-        }, {
-          removeDesc: false
-        }, {
-          removeXMLNS: true
-        }, {
-          removeUnknownsAndDefaults: {
-            keepRoleAttr: true
-          }
-        }, {
-          removeViewBox: false
-        }, {
-          removeDimensions: true
-        }, {
-          removeAttrs: {
-            attrs: 'fill'
-          }
-        }]
-      }
-    ))
-    .pipe(gulp.dest('src/svg'));
-});
 
 // // browserSync and file watching
 gulp.task('serve', function () {
   browserSync.init({
-    server: site
+    server: 'dist'
     // browser: 'FireFox'
   });
 
+  gulp.watch(['src/pug/*.pug', 'src/svg/*.svg'], gulp.series('pug'));
+  gulp.watch('src/sass/*.scss', gulp.series('sass'));
   gulp.watch('src/js/*.js', gulp.series('js'));
-  gulp.watch(['src/stylus/*.styl', 'src/stylus/*.css'], gulp.series('stylus'));
-  gulp.watch(['src/pug/*.pug'], gulp.series('pug'));
-  // gulp.watch('src/images/*.svg', gulp.series('svgo', 'pug'));
-  gulp.watch([site + 'index.html', site + 'js/*.js']).on('change', browserSync.reload);
+  gulp.watch(['dist/index.html', 'dist/js/*.js']).on('change', browserSync.reload);
 });
 
 gulp.task('default', gulp.series('serve'));
+gulp.task('build', gulp.parallel('root', 'favicons', 'fonts', 'images', 'videos', 'pug', 'sass', 'js'));
